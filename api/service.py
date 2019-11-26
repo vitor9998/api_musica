31b5f8dd963c7b6f5276991b8df292b9221da64f
Vitor Oikawa <vitor.oikawa@gitlab.com>
2019-11-26 15:53:13 -0300

    atualizado

--- api/service.py
+++ api/service.py
@@ -6,27 +6,15 @@ class Musicas:
     
     def filtro(nome):
         
-        nome = nome.replace("","-")
+        nome = nome.replace(" ","-")
         URL = 'http://api.genius.com/search/' + str(nome)
-        TOKEN = 'NnasQ_313kmVSFmWp9fujcP7GgP_QQ1kHK1ho-fiAgW7heMhcmjGXmsSj-bIG0zh'
+        TOKEN = 'gcBfPOSLKKx4BXKq5w6R1zzBkj154sO3V-wuYkcOHM97Z_wFeKM4sjkPkYudDj0t'
         acess = 'Bearer {}'.format(TOKEN)
         headers = {'Authorization': acess}    
    
-        resposta = requests.request('GET',URL,headers=headers)
-        lista_de_musicas = resposta.json()['response']['songs']
-   
-    
-        top_10 = []
-        x=1 
-        if len(lista_de_musicas) >= 10:
-            for musica in lista_de_musicas[:10]:
-            
-                x = x+1
-                top_10.append(unicodedata.normalize("NFKD",musica['full_title']))
- 
-        else:
-            for musica in lista_de_musicas:
-                top_10.append(unicodedata.normalize("NFKD",musica['full_title']))
+        resposta = requests.get(URL,headers=headers)
+        lista_de_musicas = resposta.text
+        
 
     
 
