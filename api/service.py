bb3cd2100f4db3067246890d3cbb643d6d48b043
Vitor Oikawa <vitor.oikawa@gitlab.com>
2019-11-27 11:13:42 -0300

    atualizado

--- api/service.py
+++ api/service.py
@@ -5,19 +5,24 @@ import unicodedata
 class Musicas:
     
     def filtro(nome):
+        nome = nome.replace(" ", "-")
         
-        nome = nome.replace(" ","-")
-        URL = 'http://api.genius.com/search/' + str(nome)
+        URL = 'http://api.genius.com/search/'
         TOKEN = 'gcBfPOSLKKx4BXKq5w6R1zzBkj154sO3V-wuYkcOHM97Z_wFeKM4sjkPkYudDj0t'
         acess = 'Bearer {}'.format(TOKEN)
         headers = {'Authorization': acess}    
-   
-        resposta = requests.get(URL,headers=headers)
+
+        parametros ={'q': nome}
+        resposta = requests.get(URL,params=parametros,headers=headers)
+
+
         lista_de_musicas = resposta.text
         
 
     
+       
+        json_retorno = json.dumps(lista_de_musicas)
 
-        return lista_de_musicas
+        return json_retorno
 
        
